0974edfc276f9a675c21b234eb228a06af81adab
learning Git <sachinkumar270702@gmail.com>
2025-01-05 18:46:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './Components/Navbar'
+import Main from './Components/Main'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Main/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+    return (
+        <header className="inter-head">
+            <nav>
+                <img src="./src/assets/images/react-logo.png" alt="React_Logo" />
+                <span >React Facts</span>
+            </nav>
+        </header>
+
+    )
+}
